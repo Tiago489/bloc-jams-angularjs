b024abd57d271bd9d93c5f6561dbc92b75ebe763
Cloudbreaker489 <tiagolopes489@gmail.com>
2018-11-20 15:07:10 -0500

    added the Fixtures service

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,16 +1,11 @@
 (function() {
     function CollectionCtrl() {
 
-        /* albums property defined as an empty array */
-        this.albums = [];
+        this.albums = Fixtures.getCollection(12);
 
-        /* we are creating 12 copies of the albumPicasso located within the fixtures.js file */
-        for (var i = 0; i < 12; i++) {
-            this.albums.push(angular.copy(albumPicasso));
-        }
     }
 
     angular
         .module('blocJams')
-        .controller('CollectionCtrl', CollectionCtrl);
+        .controller('CollectionCtrl', ['Fixtures', CollectionCtrl]);
 })();
